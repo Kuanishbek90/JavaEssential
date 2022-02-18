e8ddab3577d6b08a1fa5d7fcfddbb206419e9b84
Quanishbek <kuanysbekismanov@gmail.com>
2022-02-18 23:27:32 +0500

    JAVA essential HomeWork6.1

--- homework6/task2/Main.java
+++ homework6/task2/Main.java
@@ -2,10 +2,10 @@ package homework6.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Vehicle.Wheel whell = new Vehicle().new Wheel();
-        Vehicle.Door door = new Vehicle().new Door();
-        whell.printWheelColour();
-        door.printDoorColour();
+        Vehicle vehicle = new Vehicle();
+        vehicle.new Wheel().printWheelColour();
+        vehicle.new Door().printDoorColour();
+
 
     }
 }
--- homework6/task2/Vehicle.java
+++ homework6/task2/Vehicle.java
@@ -1,18 +1,19 @@
 package homework6.task2;
 
 public class Vehicle {
-    public void printColour() {
-    }
+
 
     class Wheel {
+        private String colour = "Red";
         public void printWheelColour() {
-            System.out.println("Green");
+            System.out.println(colour);
         }
     }
 
     class Door {
+        private String colour = "Green";
         public void printDoorColour() {
-            System.out.println(" Red ");
+            System.out.println(colour);
         }
     }
 }
